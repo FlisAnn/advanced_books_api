34c66ac6dab909e5d2aaedeec84106f14c0088a3
flis_ann <ann.flismark@gmail.com>
2021-01-22 13:19:13 +0100

    Trying to install db

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/booksController')
 
-/* GET users listing. */
 router.get('/', booksController.index);
 
 module.exports = router;
